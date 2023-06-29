5e260ff8f94d8b522b0a8bbd8fcda3f87bfce491
Vyachuk <Slavkoka045@gmail.com>
2023-06-29 14:29:34 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0,
+    bad: 0, //hello
   };
   handleAddMark = key => {
     this.setState(prevState => ({
